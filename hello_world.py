21b918f4c46cadf3aea708d2f75601cc7f035bf1
Mikko <mikko.luhtasaari@tuni.fi>
2020-01-17 13:45:59 +0200

    Add prints
    Line change
    
    test2
    
    commit
    
    qwerty
    
    fix typo

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 print("Hello world!")
-
-
 print("Hello darkness my old friend")
 print("I've come to talk with you again")
+print("Not another hello world")
+print("No")
+print("More")
+print("Hello")
+print("World")
+print("Please, I'm begging you")
